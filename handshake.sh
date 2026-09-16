b1cee8eaf0dd86cc1f47575b31431402bd188727
sirhellme <sirhellme@gmail.com>
2026-09-17 00:53:57 +0800

    testing the read code

--- handshake.sh
+++ handshake.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
+echo "Seeing your ip adress..."
 ip addr
 iw dev
 
+echo "Starting airmon-ng..."
 sudo airmon-ng check kill
-sudo airmon-ng start wlan0
-iw dev
+
+echo "what's your wlan? "
+read wlan
+
+sudo airmon-ng start $wlan
+iw dev # cheking
+
+echo "your mon wlan? eg:wlan0mon: "
+read mon
+sudo airodump-ng $mon
+
+# we need to see the BSSID, Channel, and ESSID
+# and then do the thing
